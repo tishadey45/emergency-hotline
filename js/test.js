fb253876dc44e1a26117754cbc7e2ba46877356b
Tisha Dey <tishadey2025@outlook.com>
2026-02-19 23:54:57 +0600

    :tada: dynamic call history implemented

---
+++ js/test.js
@@ -0,0 +1,34 @@
+// selecting all call buttons
+const callButtons = document.querySelectorAll(".call-button");
+// selecting the call history container
+const callHistoryContainer = document.getElementById("call-history-container");
+
+// adding event listener to each call button
+callButtons.forEach((button) => {
+  button.addEventListener("click", function (event) {
+    // console.log("clicked", button);
+    // find the parent card of the clicked button
+    const card = event.target.closest(".card");
+    const serviceName = card.querySelector(".service-name").textContent;  //innerText or textContent or innerHTML
+    const serviceNumber = card.querySelector(".service-number").textContent;
+    const now = new Date();
+    // const timestamp = now.toLocaleString();
+    const currentTime = now.toLocaleTimeString();
+    // console.log({ serviceName, serviceNumber, currentTime });
+
+    // add new call history item
+    const historyItem = document.createElement("div");
+    historyItem.className =
+      "flex justify-between items-center mt-4 p-4 bg-gray-50 rounded-2xl";
+
+    historyItem.innerHTML = `
+                           <div class="space-y-1">
+                                   <h6 class="text-sm font-medium">${serviceName}</h6>
+                                   <h2 class="text-lg ">${serviceNumber}</h2>
+                            </div>
+                            <p class="text-sm text-gray-500">${currentTime}</p>
+`;
+// callHistoryContainer.appendChild(historyItem);
+callHistoryContainer.prepend(historyItem);
+  });
+});
